90175f8532743e38ed73445c4e679e599a0774db
fakaha <lunastra34@gmail.com>
2023-07-26 02:37:54 +0700

    add: features update todo list

--- src/modul/TodoListApp.jsx
+++ src/modul/TodoListApp.jsx
@@ -2,21 +2,40 @@ import { useState } from "react";
 
 const TodoListApp = () =>{
     const [activity, setActivity] = useState('');
+    const [edit, setEdit] = useState({});
     const [todos, setTodos] = useState([]);
 
     function generateId(){
         return Date.now();
     }
 
-    function addTodoHandler (event){
+    function saveTodoHandler (event){
         event.preventDefault();
+
+        if(edit.id){
+            const updatedTodo = {
+                id: edit.id,
+                activity: activity
+            };
+            
+            const editTodoIndex = todos.findIndex(function(todo){
+                return todo.id == edit.id
+            });
+
+            const updatedTodos = [...todos]
+            updatedTodos[editTodoIndex] = updatedTodo;
+            
+            setTodos(updatedTodos);
+
+            return;
+        }
 
         // setTodos([...todos, activity]);
         setTodos([
             ...todos, 
             {
                 id: generateId(),
-                activity: activity,
+                activity: activity, //Bisa disingkat activity saja kalau properti dan value sama
             },
         ]);
         setActivity('');
@@ -30,10 +49,15 @@ const TodoListApp = () =>{
         setTodos(filteredTodos);
     }
 
+    function editTodoHandler(todo){
+        setActivity(todo.activity)
+        setEdit(todo);
+    }
+
     return(
         <>
         <h1>Simple Todo List</h1>
-        <form onSubmit={addTodoHandler}>
+        <form onSubmit={saveTodoHandler}>
             <input 
             type="text" 
             placeholder="Nama aktivitas"
@@ -41,12 +65,15 @@ const TodoListApp = () =>{
             onChange={function(event){
                 setActivity(event.target.value)
             }}/>
-            <button type="submit">Tambah</button>
+            <button type="submit">
+                {edit.id ? 'Simpan Perubahan' : 'Tambah'}
+            </button>
         </form>
         <ul>
             {todos.map(function(todo){
                 return( 
                 <li key={todo.id}>{todo.activity}
+                <button onClick={editTodoHandler.bind(this, todo)}>Edit</button>
                 <button onClick={removeTodoHandler.bind(this, todo.id)}>Hapus</button>
                 </li>
                 );
